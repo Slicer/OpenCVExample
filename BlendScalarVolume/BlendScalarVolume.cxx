a8a32c2b2e673a784a57b417df370e0c8d7deb28
Nicole Aucoin <nicole@bwh.harvard.edu>
2016-03-07 15:58:01 -0500

    ENH: first commit of OpenCVExample
    This is an extension that depends on the SlicerOpenCV
    extension in order to access OpenCV functionality. It
    provides a C++ CLI module that takes two input vector images
    as png files and blends them together.
    
    Issue https://github.com/SBU-BMI/SlicerOpenCV/issues/6

---
+++ BlendScalarVolume/BlendScalarVolume.cxx
@@ -0,0 +1,61 @@
+#include "BlendScalarVolumeCLP.h"
+
+#include <opencv2/opencv.hpp>
+
+// Use an anonymous namespace to keep class types and function names
+// from colliding when module is used as shared object module.  Every
+// thing should be in an anonymous namespace except for the module
+// entry point, e.g. main()
+//
+namespace
+{
+} // end of anonymous namespace
+
+
+int main( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+  double beta;
+  cv::Mat source1, source2, destination;
+
+  std::cout << "alpha = " << alpha << std::endl;
+  std::cout << "gamma = " << gamma << std::endl;
+  beta = 1.0 - alpha;
+  std::cout << "beta = " << beta << std::endl;
+  std::cout << "inputVolume1 = " << inputVolume1.c_str() << std::endl;
+  std::cout << "inputVolume2 = " << inputVolume2.c_str() << std::endl;
+  std::cout << "outputVolume = " << outputVolume.c_str() << std::endl;
+
+  // read in the input images using the OpenCV image reader
+  source1 = cv::imread(inputVolume1.c_str());
+  if (!source1.data)
+    {
+    std::cerr << "Error reading input volume 1 " << inputVolume1.c_str() << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  source2 = cv::imread(inputVolume2.c_str());
+  if (!source2.data)
+    {
+    std::cerr << "Error reading input volume 2 " << inputVolume2.c_str() << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // do the blending
+  cv::addWeighted(source1, alpha, source2, beta, gamma, destination);
+
+  if (!destination.data)
+    {
+    std::cerr << "Error: no output from the blending!" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // write output
+  if (!cv::imwrite(outputVolume.c_str(), destination))
+    {
+    std::cerr << "Failed to write output image " << outputVolume.c_str() << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# OpenCVExample
+
+This is work in progress to develop a sample extension that depends on the SlicerOpenCV
+extension (https://github.com/SBU-BMI/SlicerOpenCV) for 3D Slicer (http://slicer.org).
+It shows how to create modules that depend on OpenCV (http://opencv.org/) through
+teh SlicerOpenCV extension.
+
+See this Slicer labs page for details on what features will be provided by the
+extension, design decisions and such:
+
+http://wiki.slicer.org/slicerWiki/index.php/Documentation/Labs/OpenCV
+
+# Contact
+
+If you would like to use OpenCV libraries in 3D Slicer, you can contact us to
+communicate your requirements, and we will consider them in our development.
+
+* Lead developer: Nicole Aucoin (https://github.com/naucoin)
+* Project PI: Andrey Fedorov (https://github.com/fedorov or
+  andrey.fedorov@gmail.com)
+* Contributors:
+ * Jean-Christophe Fillion-Robin (https://github.com/jcfr)
+
+You can also post your questions or considerations to the 3D Slicer developer
+list: http://massmail.spl.harvard.edu/mailman/listinfo/slicer-devel
+
